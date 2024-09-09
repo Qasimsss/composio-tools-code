bc5b0279bc0349f32694c7199d73d4285e962908
Qasim Javed <suriyafamily143@gmail.com>
2024-09-09 12:34:55 -0700

    .

---
+++ google_calendar.py
@@ -0,0 +1,24 @@
+from composio_openai import ComposioToolSet, Action
+from litellm import completion
+
+composio_key = "x47gotu8ung46fm3gfsncx"
+
+
+
+composio_toolset = ComposioToolSet(entity_id="default" , api_key=composio_key)
+
+tools = composio_toolset.get_tools(actions=[Action.GOOGLECALENDAR_CREATE_EVENT])
+
+task = "Create a 1 hour meeting event at 5:30PM tomorrow"
+
+response = completion(
+    model="gpt-4o-mini",
+    tools=tools,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": task},
+    ],
+)
+
+result = composio_toolset.handle_tool_calls(response)
+print(result)
